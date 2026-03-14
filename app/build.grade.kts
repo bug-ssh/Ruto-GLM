ffd9d1d2802f412c2449596339710571774b906e
天空好像下雨 <151932354+bug-ssh@users.noreply.github.com>
2026-03-15 01:37:33 +0800

    Update versionName to '1.01'

--- app/build.grade.kts
+++ app/build.grade.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.01"
 
         ksp {
             arg("room.schemaLocation", "$projectDir/schemas")
